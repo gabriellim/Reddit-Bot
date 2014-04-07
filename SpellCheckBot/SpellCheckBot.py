6f7aaffc9e9d5bf8ae5f08ee05e598956fc4ca3f
Gabriel Lim <gabriel_lim@sfu.ca>
2014-04-06 18:27:06 -0700

    v1.1.6 Updated banlist
    Updated reason for being banned

--- SpellCheckBot/SpellCheckBot.py
+++ SpellCheckBot/SpellCheckBot.py
@@ -9,12 +9,12 @@ SpellCheckBot by /u/ddNTP
 -----------------------------------------------------------------------------------------------------------------------------------------------
 
 /r/nfl              28 March 2014 @ ~6:40  PM       We don't allow novelty accounts      http://www.reddit.com/message/messages/1pl3bu
-/r/funny            28 March 2014 @ ~7:01  PM       Bots aren't allowed in /r/funny      http://www.reddit.com/message/messages/1p8gq9
+/r/funny            28 March 2014 @ ~7:01  PM       Bots are not allowed                 http://www.reddit.com/message/messages/1p8gq9
 /r/leagueoflegends  28 March 2014 @ ~7:16  PM       We don't allow novelty bots          http://www.reddit.com/message/messages/1p8gww
 /r/xboxone          31 March 2014 @ ~7:01  AM       We don't want junk bots              http://www.reddit.com/message/messages/1pitsw
 /r/asoiaf            3 April 2014 @ ~12:31 PM       Pending reply                        http://www.reddit.com/message/messages/1pwd26
 /r/relationships     6 April 2014 @ ~2:25  PM       Pending reply                        http://www.reddit.com/message/messages/1q8wyp
-/r/seireitei         6 April 2014 @ ~4:24  PM       Pending reply                        http://www.reddit.com/message/messages/1q9di0
+/r/seireitei         6 April 2014 @ ~4:24  PM       Bots are not allowed                 http://www.reddit.com/message/messages/1q9di0
 
 --------------------
        Stats:
